13f8fe7d3118dc72a5eb9e6eb3119ac3f6ca2856
fil <fil.src@gmail.com>
2015-04-08 00:44:53 +0300

    tree dummy component

---
+++ src/main/java/ru/fly/client/ui/tree/Tree.java
@@ -0,0 +1,16 @@
+package ru.fly.client.ui.tree;
+
+import com.google.gwt.user.client.DOM;
+import ru.fly.client.ui.Component;
+
+/**
+ * User: fil
+ * Date: 05.03.15
+ */
+public class Tree extends Component {
+
+    public Tree() {
+        super(DOM.createDiv());
+    }
+
+}
