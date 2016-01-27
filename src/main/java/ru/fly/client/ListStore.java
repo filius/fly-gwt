441bc962a6202175da12b0895597ac1d68671bbe
fil <fil.src@gmail.com>
2016-01-27 22:35:25 +0300

    added replace in ListStore

--- src/main/java/ru/fly/client/ListStore.java
+++ src/main/java/ru/fly/client/ListStore.java
@@ -31,99 +31,111 @@ import java.util.List;
  */
 public class ListStore<T> extends EventBase {
 
-    public interface StoreFilter<T>{
+    public interface StoreFilter<T> {
         boolean isVisible(T item);
     }
 
     private List<T> list = new ArrayList<T>();
     private List<StoreFilter<T>> filters = new ArrayList<StoreFilter<T>>();
 
-    public ListStore(){}
+    public ListStore() {
+    }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return list == null || list.size() < 1;
     }
 
-    public void clear(){
+    public void clear() {
         list.clear();
         fireEvent(new UpdateEvent());
     }
 
-    public void addAll(Collection<T> list){
+    public void addAll(Collection<T> list) {
         this.list.addAll(list);
         fireEvent(new UpdateEvent());
     }
 
-    public void add(T item){
+    public void add(T item) {
         list.add(item);
         fireEvent(new UpdateEvent());
     }
 
-    public void remove(T item){
-        if(list.remove(item))
+    public void remove(T item) {
+        if (list.remove(item)) {
             fireEvent(new UpdateEvent());
+        }
     }
 
-    public void insert(int idx, T item){
-        if(list.contains(item)){
-            list.remove(item);
-        }
+    public void insert(int idx, T item) {
+        list.remove(item);
         list.add(idx, item);
         fireEvent(new UpdateEvent());
     }
 
-    public int indexOf(T item){
+    public void replace(T item) {
+        int idx = list.indexOf(item);
+        if(idx > -1) {
+            list.remove(item);
+            list.add(idx, item);
+        }else{
+            list.add(item);
+        }
+        fireEvent(new UpdateEvent());
+    }
+
+    public int indexOf(T item) {
         return list.indexOf(item);
     }
 
-    public boolean contains(T item){
+    public boolean contains(T item) {
         return list.contains(item);
     }
 
     /**
      * exchange two element of store
+     *
      * @param idx1 - element one
      * @param idx2 - element two
      */
-    public void exchange(int idx1, int idx2){
+    public void exchange(int idx1, int idx2) {
         T tmp = list.get(idx1);
         list.set(idx1, list.get(idx2));
         list.set(idx2, tmp);
         fireEvent(new UpdateEvent());
     }
 
-    public int size(){
+    public int size() {
         return getList().size();
     }
 
-    public T get(int idx){
+    public T get(int idx) {
         return getList().get(idx);
     }
 
-    public T get(T model){
+    public T get(T model) {
         return list.get(list.indexOf(model));
     }
 
-    public void fill(Collection<T> list){
+    public void fill(Collection<T> list) {
         this.list.clear();
-        if(list != null)
+        if (list != null)
             this.list.addAll(list);
         fireEvent(new UpdateEvent());
     }
 
-    public List<T> getList(){
+    public List<T> getList() {
         List<T> view = new ArrayList<T>();
-        if(list != null){
-            if(filters.size() > 0){
-                for(T item : list){
+        if (list != null) {
+            if (filters.size() > 0) {
+                for (T item : list) {
                     boolean visible = true;
-                    for(StoreFilter<T> f : filters){
+                    for (StoreFilter<T> f : filters) {
                         visible &= f.isVisible(item);
                     }
-                    if(visible)
+                    if (visible)
                         view.add(item);
                 }
-            }else
+            } else
                 view.addAll(list);
         }
         return view;
@@ -138,11 +150,11 @@ public class ListStore<T> extends EventBase {
 //        return null;
 //    }
 
-    public void addFilter(StoreFilter<T> filter){
+    public void addFilter(StoreFilter<T> filter) {
         filters.add(filter);
     }
 
-    public HandlerRegistration addUpdateHandler(UpdateEvent.UpdateHandler lnr){
+    public HandlerRegistration addUpdateHandler(UpdateEvent.UpdateHandler lnr) {
         return addHandler(lnr, UpdateEvent.getType());
     }
 
